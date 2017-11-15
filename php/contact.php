b93d146ecbf4fe2ed730ec2e3ce2c4855f306786
Shilei Lin <lin.shilei@outlook.com>
2017-11-15 13:34:56 +0800

    Revert "start new version"
    This reverts commit b67fe3c

--- php/contact.php
+++ php/contact.php
@@ -38,7 +38,8 @@ if(get_magic_quotes_gpc()) {
 // Enter the email address that you want to emails to be sent to.
 // Example $address = "joe.doe@yourdomain.com";
 
-$address = "lin.shilei@outlook.com";
+//$address = "example@example.net";
+$address = "example@example.net";
 
 
 // Configuration option.
